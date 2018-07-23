0648a4d1b47ee0a8b22e87c80e77dafbf1b59b77
mynameismaxz <sirachat.sir@gmail.com>
2018-07-23 16:23:35 +0700

    initial commit

---
+++ BackgroundStuff/MyBackgroundTask.cs
@@ -0,0 +1,26 @@
+﻿using Windows.Data.Xml.Dom;
+using Windows.ApplicationModel.Background;
+using Windows.UI.Notifications;
+
+namespace BackgroundStuff
+{
+    public sealed class MyBackgroundTask : IBackgroundTask
+    {
+        public MyBackgroundTask()
+        {
+        }
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            // simple example with a Toast, to enable this go to manifest file
+            // and mark App as TastCapable - it won't work without this
+            // The Task will start but there will be no Toast.
+            var toastTemplate = ToastTemplateType.ToastText02;
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(toastTemplate);
+            XmlNodeList textElements = toastXml.GetElementsByTagName("text");
+            textElements[0].AppendChild(toastXml.CreateTextNode("My first Task - Yeah"));
+            textElements[1].AppendChild(toastXml.CreateTextNode("I'm a message from your background task!"));
+            ToastNotificationManager.CreateToastNotifier().Show(new ToastNotification(toastXml));
+        }
+    }
+}
---
+++ BackgroundThread_Tutorial/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.ApplicationModel.Background;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BackgroundThread_Tutorial
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    /// refer : https://www.tutorialspoint.com/windows10_development/windows10_development_background_execution.htm
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            var access = await BackgroundExecutionManager.RequestAccessAsync();
+
+            switch (access)
+            {
+                case BackgroundAccessStatus.Unspecified:
+                    break;
+                case BackgroundAccessStatus.AllowedMayUseActiveRealTimeConnectivity:
+                    break;
+                case BackgroundAccessStatus.AllowedWithAlwaysOnRealTimeConnectivity:
+                    break;
+                case BackgroundAccessStatus.Denied:
+                    break;
+                default:
+                    break;
+            }
+
+            var task = new BackgroundTaskBuilder
+            {
+                Name = "Test Task",
+                TaskEntryPoint = typeof(BackgroundStuff.MyBackgroundTask).ToString()
+            };
+
+            var trigger = new ApplicationTrigger();
+            task.SetTrigger(trigger);
+
+            await trigger.RequestAsync();
+        }
+    }
+}
